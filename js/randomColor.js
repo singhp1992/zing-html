a718ebea6e959cc58b7682bb4c15308d42d5bc72
Preeti <singhp1992@gmail.com>
2019-04-01 15:25:30 +0200

    playing around with random color generator

--- js/randomColor.js
+++ js/randomColor.js
@@ -1,22 +1,22 @@
 //adding randoming color function to add to text later on
-var css = '';
-var rgbColor;
+// var css = '';
+// var rgbColor;
 
-function random() {
-    return Math.floor(Math.random() * 256);
-}
+// function random() {
+//     return Math.floor(Math.random() * 256);
+// }
 
-function randomColor() {
-    var color = 'rgb(';
-    color += random() + ',';
-    color += random() + ',';
-    color += random() + ')';
-    return color
-}
+// function randomColor() {
+//     var color = 'rgb(';
+//     color += random() + ',';
+//     color += random() + ',';
+//     color += random() + ')';
+//     return color
+// }
 
-for ( var i = 1; i <= 10; i+=1) {
-    rgbColor = randomColor();
-    css += rgbColor
-}
+// for ( var i = 1; i <= 10; i+=1) {
+//     rgbColor = randomColor();
+//     css += rgbColor
+// }
 
-console.log(css)
+// console.log(css)
